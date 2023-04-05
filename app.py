be0671ad632e79b39429d7c78acd36362b1d8c45
ideasodaa <nichaphat.id@gmail.com>
2023-04-05 14:51:52 +0700

    add file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 from fastapi import FastAPI, Path, Body
+=======
+from fastapi import FastAPI,Path,body
+>>>>>>> 22db0a2 (change app.py)
 import uvicorn
 from pydantic import BaseModel, Field
 
 app = FastAPI(title="Sample FastAPI",
               description="For education", version="1.0")
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 data = {1: "John", 2: "jane"}
 
@@ -31,7 +36,27 @@ def add_one_people(data: People):
 def read_root():
     return {"Hello": "Khun"}
 >>>>>>> 8e7f2ff ( change world to Khun)
+=======
+>>>>>>> 22db0a2 (change app.py)
 
+data={1:"Yuta",2:"Gigi"}
+
+class people(BaseModel):
+    id:int =Field(..., descripyion="first_name")
+    name:str= Field(None, descripyion="last_name")
+
+@app.get("/api/hello")
+def read_root():
+    return data
+
+@app.post("api/people-add")
+def add_one_people(data:people):
+    print(data)
+    return data
+
+@app.get("/api/hello_") #protocol to update
+def read_root(id:int):
+    return id,data[id]
 
 if __name__ == "__main__":
 	uvicorn.run("app:app", port=5000, log_level="info", reload=True, host="0.0.0.0")
